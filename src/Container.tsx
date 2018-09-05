484f81e83d0e0037672f8385bf50e1333a6951e3
k.tashiro <tashiro@lepra.jp>
2018-09-05 19:00:02 +0900

    analyser exercise

--- src/Container.tsx
+++ src/Container.tsx
@@ -3,17 +3,13 @@ import {
   Card,
 } from '@blueprintjs/core';
 import './handler.css';
-import Sources from './Sources';
-import Filters from './Filters';
-import Effectors from './Effectors';
+
 
 export default class Container extends React.Component {
   render() {
     return (
       <Card >
-        <Sources />
-        <Filters />
-        <Effectors />
+how r u
       </Card>
     );
   }
--- src/client.tsx
+++ src/client.tsx
@@ -1,23 +1,76 @@
+import { ChangeEvent } from "react";
+
 'use strict';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from './Container';
-import Instruments from './Instulments';
+// import Container from './Container';
+// import Instruments from './Instulments';
 
 window.onload = () => {
+  App.init();
+  const container = document.getElementById('container');
+  ReactDOM.render(<WaveFilePicker />, container);
+}
 
-  /* vender-prefix fallback */
-  // window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  // navigator.getUserMedia =
-  //   navigator.getUserMedia ||
-  //   navigator.webkitGetUserMedia ||
-  //   navigator.mozGetUserMedia;
+class App {
+  static cx: AudioContext;
+  static init() {
+    App.cx = new AudioContext();
+  }
+  static readWaveFile(audioFile: File) {
+    const fr: FileReader = new FileReader();
+    fr.addEventListener('load', () => {
+      if (!(fr.result instanceof ArrayBuffer)) { return false }
+      this.asyncDecodeAudioData(fr.result);
+    }, false);
+    fr.readAsArrayBuffer(audioFile);
+  }
+  private static asyncDecodeAudioData(data: ArrayBuffer) {
+    App.cx.decodeAudioData(data, (decoded: AudioBuffer) => {
+      const fileSource = App.cx.createBufferSource();
+      fileSource.buffer = decoded;
 
-  Instruments.init();
-  Instruments.connect()
-    .then(() => {
-      const container = document.getElementById('container');
-      ReactDOM.render(<Container />, container);
-    });
+      const analyser = App.cx.createAnalyser();
+      const scriptProcessor = App.cx.createScriptProcessor(analyser.fftSize, 1, 1);
+      fileSource.connect(analyser);
+
+      analyser.connect(scriptProcessor);
+      scriptProcessor.connect(App.cx.destination);
+
+      scriptProcessor.onaudioprocess = App.onAudioProcessHandler.bind(this, analyser);
+
+      fileSource.start(0);
+    })
+  }
+
+  static onAudioProcessHandler(analyser: AnalyserNode) {
+    const amplitudeArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteTimeDomainData(amplitudeArray);
+
+    console.log(amplitudeArray);
+  }
+}
+
+class WaveFilePicker extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="file" onChange={this.onChangeHandler.bind(this)} />
+      </div>
+    )
+  }
+
+  onChangeHandler(e: ChangeEvent) {
+    const target = e.currentTarget;
+    if (!(target instanceof HTMLInputElement)) {
+      return false
+    }
+    const { files }: any = e.target
+    if (files.length === 0) {
+      return false;
+    }
+    const waveFile: File = files[0];
+    App.readWaveFile(waveFile);
+  }
 }
