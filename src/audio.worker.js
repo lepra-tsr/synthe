482cbf1e434b96d746ff1c72640bb4970d66608c
k.tashiro <tashiro@lepra.jp>
2018-09-06 16:07:56 +0900

    sample audio worker

--- src/audio.worker.js
+++ src/audio.worker.js
@@ -5,11 +5,21 @@ class Bypass extends AudioWorkletProcessor {
     super(...args);
   }
 
+  /**
+   * 
+   * @param {Float32Array[][]} inputs 
+   * @param {Float32Array[][]} outputs
+   */
   process(inputs, outputs) {
     const input = inputs[0];
     const output = outputs[0];
-    const channels = 2
     for (let i = 0; i < output.length; i++) {
+      for (let j = 0; j < output[i].length; j++) {
+        if (j % 1 !== 0) {
+          /* drop condition */
+          input[i][j] = 0;
+        }
+      }
       output[i].set(input[i]);
     }
     return true
